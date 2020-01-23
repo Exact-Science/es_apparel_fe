a3ba985a0defe275cc7a0888fc320d381f6c3650
Daniel Jordan <danieljordan86@gmail.com>
2020-01-23 16:10:51 -0700

    Eslints things

--- src/components/review-components/review/review.component.jsx
+++ src/components/review-components/review/review.component.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import './review.styles.scss';
 
-const Review = function() {
-  return (
-    <div>Daniel is the man!</div>
-  )
-}
+const Review = () => (
+  <div> Daniel is the man! </div>
+);
 
 export default Review;
