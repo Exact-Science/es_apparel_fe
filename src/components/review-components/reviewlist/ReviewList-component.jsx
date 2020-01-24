ca26b33837967a6f666aaef1cb48100e31d0f2b3
Daniel Jordan <danieljordan86@gmail.com>
2020-01-24 10:57:14 -0700

    Configures proptypes and setsState

---
+++ src/components/review-components/reviewlist/ReviewList-component.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ReviewList = (props) => {
+  return (
+    <ul>
+      <li>Review 1 </li>
+      <li>Review 2 </li>
+      <li>Review 3 </li>
+    </ul>
+  );
+};
+
+export default ReviewList;
