d9b07ec361166dfdbeb7f5e852d559eec9f229cf
Brian Bouchard <b.bouch@gmail.com>
2020-01-23 20:43:11 -0700

    fetch questions and set state

---
+++ src/components/qna-components/qna-List/qna-List.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './qna-List.styles.scss';
+
+const QnAList = () => (
+  <div>QnA</div>
+);
+
+export default QnAList;
+
--- src/components/qna-components/qna/qna.component.jsx
+++ src/components/qna-components/qna/qna.component.jsx
@@ -1,8 +1,32 @@
 import React from 'react';
 import './qna.styles.scss';
 
-const QnA = () => (
-  <div>QnA</div>
-);
+class QnA extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      list: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch(`http://3.134.102.30/qa/${this.props.id}`)
+      .then((results) => { return results.json(); })
+      .then((list) => { this.setState({ list: list.results }); })
+  }
+
+  render() {
+    const { list } = this.state;
+    return (
+      <div>
+        QnA
+        {/* <div>{ list[0].question_id }</div> */}
+        { list.map((q) => { return <div>{q.question_body}</div> }) }
+        {/* {console.log(list)} */}
+      </div>
+    );
+  }
+}
+
 
 export default QnA;
