d5cf290a073aff8dd3325767de1b011644df9369
VincentCSS <VincentHTML@gmail.com>
2020-01-24 17:31:08 -0700

    added styles to change main image and update style name

--- src/components/overview-components/styles-container/styles-container.component.jsx
+++ src/components/overview-components/styles-container/styles-container.component.jsx
@@ -2,22 +2,34 @@ import React from 'react';
 import propTypes from 'prop-types';
 
 import StyleItem from '../style-item/style-item.component';
+import StyleInfo from '../style-info/style-info.component';
 
 import './styles-container.styles.scss';
 
-const StylesContainer = ({ styles }) => (
-  <div className="styles-container">
-    {styles.map((style) => (
-      <StyleItem
-        key={`si${style.photos[0].thumbnail_url}`}
-        image={style.photos[0].thumbnail_url}
+const StylesContainer = ({ styles, changeMainImage, currentStyle }) => (
+  <>
+    <div className="style-utils">
+      <StyleInfo
+        currentStyle={currentStyle}
       />
-    ))}
-  </div>
+    </div>
+    <div className="styles-container">
+      {styles.map((style, idx) => (
+        <StyleItem
+          key={`si${style.photos[0].thumbnail_url}`}
+          idx={idx}
+          changeMainImage={changeMainImage}
+          image={style.photos[0].thumbnail_url}
+        />
+      ))}
+    </div>
+  </>
 );
 
 export default StylesContainer;
 
 StylesContainer.propTypes = {
   styles: propTypes.arrayOf(propTypes.object).isRequired,
+  changeMainImage: propTypes.func.isRequired,
+  currentStyle: propTypes.string.isRequired,
 };
