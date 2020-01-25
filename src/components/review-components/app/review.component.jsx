75fd691a4065389e2b4396365999b0df01d288bd
Daniel Jordan <danieljordan86@gmail.com>
2020-01-25 10:36:00 -0700

    Removes esLint errors

--- src/components/review-components/app/review.component.jsx
+++ src/components/review-components/app/review.component.jsx
@@ -17,30 +17,31 @@ class Reviews extends React.Component {
 
   componentDidMount() {
     const { id } = this.props;
+    const { reviews } = this.state;
 
     fetch(`http://3.134.102.30/reviews/${id}/list`)
       .then((data) => data.json())
       .then((res) => this.setState({ reviews: res.results, count: res.count }))
       .catch((err) => err)
-      .then(() => this.getRatings(this.state.reviews));
+      .then(() => this.getRatings(reviews));
 
     fetch(`http://3.134.102.30/reviews/${id}/meta`)
       .then((res) => res.json())
-      .then((res) => this.setState({ ratings: res}))
+      .then((res) => this.setState({ ratings: res }))
       .catch((err) => err);
   }
 
-  getRatings = (array) => {
-    if (array.length > 0) {
-      this.setState({ rating: array.map(el => el.rating).reduce((a, b) => (a + b) / array.length) });
-    }
-  }
+  // getRatings = (array) => {
+  //   if (array.length > 0) {
+  //     this.setState({ rating: array.map(el => el.rating).reduce((a, b) => (a + b) / array.length) });
+  //   }
+  // }
 
   render() {
     const { reviews, rating } = this.state;
     return (
       <div className="reviewsContainer">
-        <Metrics rating={rating} id={this.props.id}/>
+        <Metrics rating={rating} id={this.props.id} />
         <ReviewList reviews={reviews} />
       </div>
     );
--- src/components/review-components/listheader/listheader.component.jsx
+++ src/components/review-components/listheader/listheader.component.jsx
@@ -4,11 +4,13 @@ import propTypes from 'prop-types';
 const ListHeader = ({ number }) => (
   <h1>
     There are
-    {number}
-    reviews
+    { number }
+    reviews, sorted by relevance
   </h1>
 );
 
 ListHeader.propTypes = {
   number: propTypes.number.isRequired,
 };
+
+export default ListHeader;
