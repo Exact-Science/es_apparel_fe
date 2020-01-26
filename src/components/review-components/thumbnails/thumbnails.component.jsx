5820294ab62687c1e8f6cf41093dc5bf358f204e
Daniel Jordan <danieljordan86@gmail.com>
2020-01-26 14:20:47 -0700

    Accessing images, no rendering

--- src/components/review-components/thumbnails/thumbnails.component.jsx
+++ src/components/review-components/thumbnails/thumbnails.component.jsx
@@ -3,13 +3,11 @@ import propTypes from 'prop-types';
 
 const Thumbnails = ({ images }) => {
   if (images.length) {
-    images.map((img) => {
-      return (
-        <div className="thumbnail">
-          <img src={img.url} key={img.id} alt="product thumbnail" />
-        </div>
-      );
-    });
+    images.map((img) => (
+      <div>
+        <img className="thumbnail" src={img.url} key={img.id} alt="product thumbnail" />
+      </div>
+    ));
   }
   return null;
 };
