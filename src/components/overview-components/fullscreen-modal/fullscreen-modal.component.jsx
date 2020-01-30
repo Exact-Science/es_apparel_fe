708ecd79efaf134a7a0db6e96828bb17816c1302
VincentCSS <VincentHTML@gmail.com>
2020-01-30 11:32:02 -0700

    added image zoom on hover

--- src/components/overview-components/fullscreen-modal/fullscreen-modal.component.jsx
+++ src/components/overview-components/fullscreen-modal/fullscreen-modal.component.jsx
@@ -1,36 +1,26 @@
 import React from 'react';
 import propTypes from 'prop-types';
 
+import Magnifier from 'react-magnifier';
 import ModalButton from '../modal-button/modal-button.component';
 
 import './fullscreen-modal.styles.scss';
 
-class FullscreenModal extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
 
-    };
-  }
+const FullscreenModal = ({ handleFullscreen, mainImage, changeMainImage }) => (
+  <div className="fullscreen-modal" onClick={(e) => handleFullscreen(e)} role="presentation">
+    <div className="fullscreen-modal-content">
+      <Magnifier src={mainImage} />
+    </div>
+    <div className="modal-buttons-container">
+      <ModalButton changeMainImage={changeMainImage} />
+    </div>
+    <button type="button" id="modal-escape" onClick={(e) => handleFullscreen(e, true)}>
+        Close
+    </button>
+  </div>
+);
 
-  render() {
-    const { handleFullscreen, mainImage, changeMainImage } = this.props;
-
-    return (
-      <div className="fullscreen-modal" onClick={(e) => handleFullscreen(e)} role="presentation">
-        <div className="fullscreen-modal-content">
-          <img src={mainImage} alt="main" />
-        </div>
-        <div className="modal-buttons-container">
-          <ModalButton changeMainImage={changeMainImage} />
-        </div>
-        <button type="button" id="modal-escape" onClick={(e) => handleFullscreen(e, true)}>
-          Close
-        </button>
-      </div>
-    );
-  }
-}
 
 export default FullscreenModal;
 
