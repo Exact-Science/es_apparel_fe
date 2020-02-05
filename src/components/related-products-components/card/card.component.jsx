5aef0ca76e82614dcac0f88f56ab0731dd6e9514
Brian Bouchard <b.bouch@gmail.com>
2020-02-04 18:15:53 -0700

    show sale pricing-e.g.product19

--- src/components/related-products-components/card/card.component.jsx
+++ src/components/related-products-components/card/card.component.jsx
@@ -1,3 +1,6 @@
+/* eslint-disable react/require-default-props */
+/* eslint-disable react/jsx-one-expression-per-line */
+/* eslint-disable radix */
 import React from 'react';
 import propTypes from 'prop-types';
 import './card.styles.scss';
@@ -14,34 +17,39 @@ class Card extends React.Component {
     const { relatedProduct, productStyle } = this.props;
     return (
       <div className="rp-card-container">
-        {/* {productStyle ? console.log(productStyle[0].results[2].sale_price) : null} */}
-        {/* <div className="image"><img src={productStyle ? productStyle[0].results[0].photos[0].url : null} /></div> */}
-        {/* <div>{relatedProduct.category}</div>
-        <div>{relatedProduct.name}</div> */}
-        {/* <div>${relatedProduct.default_price}</div> */}
-        {productStyle && parseInt(productStyle[0].results[0].sale_price) > 0
-          ?
+        <div className="image"><img src={productStyle ? productStyle[0].results[0].photos[0].url : null} alt="Related Product" /></div>
+        <div>{relatedProduct.category}</div>
+        <div>{relatedProduct.name}</div>
+        {productStyle && productStyle[0].results[0].sale_price && parseInt(productStyle[0].results[0].sale_price) > 0
+          ? (
             <div className="rp-pricing-container">
-              <span className="rp-pricing-default-strikethrough">${relatedProduct.default_price}</span>
+              <span className="rp-pricing-default-strikethrough">
+                ${relatedProduct.default_price}
+              </span>
               <span className="rp-pricing-sale"> ${productStyle[0].results[0].sale_price}</span>
             </div>
-          :
-            productStyle
-              ?
-                <div className="rp-pricing-container">
-                  <span className="rp-pricing-default">${relatedProduct.default_price}</span>
-                </div>
-              :
-                null
-          }
+          )
+          : null}
+        {productStyle && !(parseInt(productStyle[0].results[0].sale_price) > 0)
+          ? (
+            <div className="rp-pricing-container">
+              <span className="rp-pricing-default">${relatedProduct.default_price}</span>
+            </div>
+          )
+          : null}
+
       </div>
     );
   }
 }
 
 Card.propTypes = {
-  relatedProduct: propTypes.shape({}).isRequired,
-  productStyle: propTypes.arrayOf(propTypes.shape({})).isRequired,
+  relatedProduct: propTypes.shape({
+    category: propTypes.string.isRequired,
+    name: propTypes.string.isRequired,
+    default_price: propTypes.string.isRequired,
+  }).isRequired,
+  productStyle: propTypes.arrayOf(propTypes.shape({})),
 };
 
 export default Card;
