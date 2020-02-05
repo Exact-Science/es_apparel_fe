21ff09bee4fba025b87bd1f5e742b47a113efdf8
Brian Bouchard <b.bouch@gmail.com>
2020-02-04 20:24:00 -0700

    organize card details

--- src/components/related-products-components/card/card.component.jsx
+++ src/components/related-products-components/card/card.component.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable react/require-default-props */
 /* eslint-disable react/jsx-one-expression-per-line */
 /* eslint-disable radix */
@@ -5,8 +6,6 @@ import React from 'react';
 import propTypes from 'prop-types';
 import './card.styles.scss';
 import Rating from '@material-ui/lab/Rating';
-import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
 
 class Card extends React.Component {
   constructor(props) {
@@ -20,17 +19,22 @@ class Card extends React.Component {
     const { relatedProduct, productStyle } = this.props;
     return (
       <div className="rp-card-container">
-        <div className="image"><img src={productStyle ? productStyle[0].results[0].photos[0].url : null} alt="Related Product" /></div>
+        <div className="image"><img
+          src={productStyle && productStyle[0].results[0].photos[0].url
+            ? productStyle[0].results[0].photos[0].url
+            : 'https://static.thenounproject.com/png/2932881-200.png'}
+          alt="Related Product"
+        />
+        </div>
         <div className="rp-card-details">
           <div>{relatedProduct.category}</div>
           <div className="product-name">{relatedProduct.name}</div>
-          {productStyle && productStyle[0].results[0].sale_price && parseInt(productStyle[0].results[0].sale_price) > 0
+          {productStyle && productStyle[0].results[0].sale_price
+          && parseInt(productStyle[0].results[0].sale_price) > 0
             ? (
               <div className="rp-pricing-container">
-                <span className="rp-pricing-sale">
-                  ${productStyle[0].results[0].sale_price}
-                </span>
-                <span className="rp-pricing-default-strikethrough"> ${relatedProduct.default_price}</span>
+                <span className="rp-pricing-sale">${productStyle[0].results[0].sale_price} </span>
+                <span className="rp-pricing-default-strikethrough">${relatedProduct.default_price}</span>
               </div>
             )
             : null}
@@ -42,21 +46,12 @@ class Card extends React.Component {
             )
             : null}
           <div>
-            <Box component="fieldset" mb={3} borderColor="transparent">
-              <Rating
-                name="simple-controlled"
-                value={5} // updated the value...value={value}
-              />
-            </Box>
+            <Rating
+              name="simple-controlled"
+              value={2.5} // updated the value...value={value}
+              precision={0.25}
+            />
           </div>
-
-
-
-
-
-
-
-
         </div>
       </div>
     );
