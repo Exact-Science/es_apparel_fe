3acece58072bfa3108ad330c8dd50038ee5661a2
Daniel Jordan <danieljordan86@gmail.com>
2020-02-09 10:03:47 -0700

    Adds a few render tests

--- src/tests/review-tests/review.test.jsx
+++ src/tests/review-tests/review.test.jsx
@@ -3,7 +3,7 @@ import Enzyme, { shallow, mount } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import RatingsChartItem from '../../components/review-components/ratings/ratingscharts/ratingschartitem/ratingschartitem.component';
 import Form from '../../components/review-components/reviews/form/form.component';
-
+import FactorItem from '../../components/review-components/ratings/factors/factoritem/factoritem.component';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -14,20 +14,25 @@ describe('Ratings chart item', () => {
   });
 });
 
+describe('Factor Item component', () => {
+  test('renders', () => {
+    const wrapper = shallow(<FactorItem />);
+    expect(wrapper.exists()).toBe(true);
+  });
+});
+
 describe('Form component', () => {
   test('renders', () => {
     const wrapper = shallow(<Form />);
     expect(wrapper.exists()).toBe(true);
   });
 
-  test('uses default form behavior for validation', () => {
-    const wrapper = shallow(<Form />);
-    const prevented = false;
-    wrapper.find('form').simulate('submit', () => {
-      preventDefault: () => {
-        prevented = true;
-      };
+  test('user text echoed in summary field', () => {
+    const wrapper = shallow(<Form handleInputChange={() => {}} />);
+
+    wrapper.find('summary').simulate('change', {
+      target: { value: 'testing' },
     });
-    expect(prevented).toBe(false);
+    expect(wrapper.find('summary').props().value).toEqual('testing');
   });
 });
